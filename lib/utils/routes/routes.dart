9fdf1a731458a4a16e784a17b12d0f0d54d8185d
Hafiz-Shehroz98 <hsaservicesofficial@gmail.com>
2023-02-02 22:02:12 +0500

    Initial commit

---
+++ lib/utils/routes/routes.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:social_media/utils/routes/route_name.dart';
+import 'package:social_media/view/splash/splash_screen.dart';
+
+class Routes {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    final arguments = settings.arguments;
+    switch (settings.name) {
+      case RouteName.splashScreen:
+        return MaterialPageRoute(builder: (_) => const SplashScreen());
+
+      default:
+        return MaterialPageRoute(builder: (_) {
+          return Scaffold(
+            body: Center(
+              child: Text('No route defined for ${settings.name}'),
+            ),
+          );
+        });
+    }
+  }
+}
---
+++ lib/view/splash/splash_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:social_media/res/fonts.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image(image: AssetImage('assets/images/logo.jpg')),
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 20),
+            child: Center(
+                child: Text(
+              'Tech Brothers Media',
+              style: TextStyle(
+                  fontFamily: AppFonts.sfProDisplayBold,
+                  fontSize: 40,
+                  fontWeight: FontWeight.w700),
+            )),
+          )
+        ],
+      )),
+    );
+  }
+}
